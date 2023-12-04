bc69cfab5ab11597af40f0cc0fc1e1ff2e155ed0
Riinaaal <riinaaal@metropolia.fi>
2023-12-04 15:43:09 +0200

    Task no.2 done(Create player in database)

--- SW2_flask.py
+++ SW2_flask.py
@@ -17,7 +17,8 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-#display score board -Su
+
+# display score board -Su
 @app.route('/score')
 def get_score():
     sql = "SELECT * FROM scoreboard ORDER BY score DESC LIMIT 50"
@@ -25,6 +26,7 @@ def get_score():
     cursor.execute(sql)
     result = cursor.fetchall()
     return json.dumps(result)
+
 
 # Sending all data of the player (if found multiple names by re-try, only the highest id will show.)
 @app.route('/<name>')
@@ -35,6 +37,7 @@ def get_player_info(name):
     result = cursor.fetchone()
     return json.dumps(result)
 
+
 # finding existing player_name -Su
 @app.route('/player/<name>')
 def get_player_name(name):
@@ -43,6 +46,20 @@ def get_player_name(name):
     cursor.execute(sql, (name,))
     result = cursor.fetchone()
     return json.dumps(result)
+
+
+# Creating a player based on name - Riina
+@app.route('/create/<name>')
+def create_name(name):
+    co2_budget = 5000000
+    co2_consumed = 0
+    total_travelled = 0
+    sql = f'''INSERT INTO player(player_name,co2_budget,co2_consumed,total_travelled)VALUES (%s,%s,%s,%s)'''
+    cursor = connection.cursor(dictionary=True)
+    val = (name, co2_budget, co2_consumed, total_travelled)
+    cursor.execute(sql, val)
+    cursor.fetchall()
+    return {'Player': 'Created'}
 
 
 # display plane types and info -Su
@@ -56,7 +73,7 @@ def get_plane_info(type):
     return json.dumps(result)
 
 
-# Reset player info, based on player_name
+# Reset player info, based on player_name - Riina
 @app.route('/re_try/<name>')
 def re_try(name):
     co2_budget = 5000000
